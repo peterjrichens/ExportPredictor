25da94ae4163c080973edb96fabc93cced3fe54e
peter <peterjrichens@gmail.com>
2016-12-29 17:55:45 +0800

    updated website

--- docs/force.js
+++ docs/force.js
@@ -15,8 +15,8 @@ function change_source() {
     var menu_value = menu.node().options[menu.node().selectedIndex].value;
     var source = path_hash[menu_value];
 
-    var w = 1000,
-    h = 650,
+    var w = 1080,
+    h = 620,
     cr = 5, // circle radius
     fill = d3.scale.category20();
 
