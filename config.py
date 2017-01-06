4bda07a6112b25be1f1e65458632fbf3c1ca1fd5
peter <peterjrichens@gmail.com>
2017-01-06 15:15:33 +0800

    commit

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # postgres login details
 
-username = 'peter'
-password = '1910'
-dbname = 'peter'
+username = ''
+password = ''
+dbname = ''
 
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://%s:%s@localhost/%s' % (username, password, dbname)
